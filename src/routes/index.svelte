826d40c80c6c8a04bed0aac3fdf0cdfe7ebe6543
Nilesh P <emailnileshp@gmail.com>
2019-05-13 21:40:04 -0400

    pwa custom app install support

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,7 +6,29 @@
 		["stackblitz.com","https://stackblitz.com/@NileshSP", "stackblitz.com/@NileshSP"],
 		["runkit.com","https://runkit.com/nileshsp","runkit.com/nileshsp"]
 	]
-	
+
+	let deferredPrompt;
+
+	const handleInstall = (e) => {
+		e.preventDefault();
+		deferredPrompt = e;
+		//btnInstallApp.style.display = 'block';
+	}
+
+	const installApp = (e) => {
+		btnInstallApp.style.display = 'none';
+		deferredPrompt.prompt();
+		deferredPrompt.userChoice
+			.then((choiceResult) => {
+				if (choiceResult.outcome === 'accepted') {
+					console.log('User accepted the A2HS prompt');
+				} else {
+					console.log('User dismissed the A2HS prompt');
+				}
+				deferredPrompt = null;
+			});
+	};
+
 </script>
 <svelte:head>
 	<title>Nilesh - Bio</title>
@@ -17,60 +39,56 @@
 		<div class="profilePhoto">
 			<img alt='Nilesh' src='favicon.png' />
 			<div class="profilePhotoCaption">Nilesh Patel</div>
-			<div class="profilePhotoPara">constantly under progress...</div>
+			<div class="profilePhotoPara">constantly improvising...</div>
 		</div>
 		<br/>
 		<div class="projects" >
-			<h4>fullstack developer with trial & errors available @</h4>
+			<h4>polygot developer with trial & errors available @</h4>
 			<div class="projectList">
 				{#each listDetails as item,i}
 					<div>{item[0]}</div><div>-</div><div><a rel="noopener, preconnect" href={item[1]} target="_blank">{item[2]}</a></div> 
 				{/each}
 			</div>
 		</div>
+		<button id="btnInstallApp" class="installAppBanner" on:click={installApp}>Install app</button>
 	</div>
 </div>
+<svelte:window on:beforeinstallprompt={handleInstall} on:appinstalled={() => console.log('app is installed by now')}/>
 <style>
 .mainContainer {
 	height:87vh;
 	color: #333;
-	font-weight:500;	
+	font-weight:500;
 }
-
 .main {
 	display:grid;
 	place-items: center;
 }
-
 @keyframes backgroundAnimation {
 	50% { transform: scale(1.05) };
 }
-
 .profilePhoto {
+	padding:2em 0em;
 	display:grid;
 	place-items: center;
 	animation: backgroundAnimation 3s infinite;
 }
-
 .profilePhoto > img {
 	max-width: 50%;
 	height:auto;
 }
-
 .profilePhotoCaption {
 	font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
 }
-
 .profilePhotoPara {
 	text-align: center;
 	font-size:80%;
 	width:40%;
 }
-
 .projects {
+	padding:1em 0em;
 	text-align: center;
 }
-
 .projectList {
 	margin: 0 auto;
 	display:grid;
@@ -81,9 +99,19 @@
 	max-width:310px;
 	font-size:80%;
 }
-
 a {
 	cursor:hand;
 }
-
+.installAppBanner {
+	display:none;
+	cursor: hand;
+	border: solid 0px orange;
+	margin:0;
+	padding:5px;
+	font-size: 8pt;
+	background: transparent;
+	border-radius: 40px;
+	box-sizing: border-box;
+	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
 </style>
